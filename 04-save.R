ea8a14ed5cb2d52fdda26c3714b8334dfdee637f
CyanaEmorrachi <78813986+CyanaEmorrachi@users.noreply.github.com>
2021-02-28 21:35:21 -0500

    Update 04-save.R

--- 04-save.R
+++ 04-save.R
@@ -1,5 +1,8 @@
 # save corrected data
-write.csv(dt1,'./data/correct/file name here',row.names = FALSE)
+write.csv(dt1,'./data/correct/data2017_rmNA.csv',row.names = FALSE)
 
-# save tidied data
-write.csv(td1t,'./data/tidy/file name here',row.names = FALSE)
+# save monthly data
+write.csv(trends,'./data/tidy/trends.csv',row.names = FALSE)
+
+# save regional data
+write.csv(regions,'./data/tidy/regions.csv',row.names = FALSE)
